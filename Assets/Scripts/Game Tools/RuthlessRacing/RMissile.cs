8f471c04f1b1fc18f61b9e40ffa6123cc740a341
SamEmam <samhoffmann111@hotmail.com>
2019-07-01 05:34:33 +0200

    Rocket Volley Powerup
    Ready coded but untested Rocket Volley Powerup

---
+++ Assets/Scripts/Game Tools/RuthlessRacing/RMissile.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RMissile : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float delayBeforeActive = 2.5f;
+    public float impactForce = 10f;
+
+    private float thrust = 10f;
+    private float radius = 10f;
+    private float upwardsThrust = 10f;
+    private float rotationSpeed = 750f;
+    private float closestDistance = float.MaxValue;
+    private bool missileEnabled = false;
+    private bool isActive = false;
+
+    [Header("Setup")]
+    public GameObject parent;
+    public Rigidbody rb;
+    public ParticleSystem thrustParticle;
+    public ParticleSystem explosion;
+    private ParticleSystem.EmissionModule emit;
+    private Transform closestTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emit = thrustParticle.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TriggerMissileCheck())
+        {
+            return;
+        }
+
+        if (!missileEnabled)
+        {
+            missileEnabled = true;
+            StartCoroutine(StartDelay());
+        }
+
+        if (isActive)
+        {
+            RotateTowardsTarget();
+            MoveForward();
+        }
+
+        // Rotate towards direction of travel
+        //transform.rotation = Quaternion.LookRotation(rb.velocity);
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeActive);
+        FindNearestTarget();
+        isActive = true;
+    }
+
+    bool TriggerMissileCheck()
+    {
+        if (transform.parent == null)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
+    void FindNearestTarget()
+    {
+        GameObject[] GOs = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject GO in GOs)
+        {
+            float dist = Vector3.Distance(GO.transform.position, transform.position);
+            if (dist < closestDistance && GO != parent)
+            {
+                closestDistance = dist;
+                closestTarget = GO.transform;
+            }
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        var direction = (closestTarget.position - transform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void MoveForward()
+    {
+        rb.AddForce(transform.forward * thrust, ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody hitRB = hit.GetComponent<Rigidbody>();
+            if (hitRB != null /*&& !hit.GetComponent<RNotAffected>()*/)
+            {
+                hitRB.AddExplosionForce(impactForce, transform.position, radius, upwardsThrust, ForceMode.Acceleration);
+            }
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject, 3f);
+    }
+}
---
+++ Assets/Scripts/Game Tools/RuthlessRacing/RMissileVolley.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class RMissileVolley : MonoBehaviour
+{
+    [Header("Properties")]
+    public float fireDelay = 0.1f;
+    public float spreadFactor = 0.5f;
+    public int playerNum;
+    public int powerupNum = 0;
+
+    private bool canShoot = true;
+
+    [Header("Setup")]
+    public GameObject[] missileGOs;
+    public RPlayerPowerup playerPowerup;
+
+    private Player player;
+    
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
+
+    private void Update()
+    {
+        if (!canShoot)
+        {
+            return;
+        }
+
+        if (player.GetButtonDown("Shoot"))
+        {
+            canShoot = false;
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        Quaternion shootDir;
+        WaitForSeconds wait = new WaitForSeconds(fireDelay);
+        foreach (var missile in missileGOs)
+        {
+            Rigidbody rb = missile.GetComponent<Rigidbody>();
+
+            shootDir = missile.transform.rotation;
+            shootDir.x += Random.Range(-spreadFactor, spreadFactor);
+            shootDir.y += Random.Range(-spreadFactor, spreadFactor);
+            missile.transform.rotation = shootDir;
+
+            missile.transform.SetParent(null);
+
+            yield return wait;
+        }
+    }
+
+}
