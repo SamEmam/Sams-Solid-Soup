a0aed1cc19e587e73f90c786dc91ccb11e69709e
SamEmam <samhoffmann111@hotmail.com>
2019-07-23 20:03:45 +0200

    Ruthless Racing Mine Launcher Implemented

---
+++ Assets/Scripts/Game Tools/RuthlessRacing/RMine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RMine : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float impactForce;
+    public float radius = 50f;
+    public float upwardsThrust = 5f;
+
+    private float delay = 2f;
+    public bool isShot = false;
+
+    [Header("Setup")]
+    public ParticleSystem explosion;
+
+    private void Update()
+    {
+        if (!isShot)
+        {
+            return;
+        }
+
+        if (delay > 0)
+        {
+            delay -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (delay > 0)
+        {
+            return;
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody hitRB = hit.GetComponent<Rigidbody>();
+                if (hitRB != null /* && !hit.GetComponent<WNotAffected>()*/)
+                {
+                    hitRB.AddExplosionForce(impactForce, transform.position, radius, upwardsThrust, ForceMode.VelocityChange);
+                }
+            }
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game Tools/RuthlessRacing/RMineLauncher.cs
@@ -0,0 +1,78 @@
+﻿using Rewired;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RMineLauncher : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float initialForce = 20f;
+    public int playerNum;
+    public int powerupNum = 1;
+
+    private int mineCount = 3;
+    private bool canShoot = true;
+
+    [Header("Setup")]
+    public Transform[] firepoints;
+    public GameObject mine;
+    public List<GameObject> mineGOs = new List<GameObject>();
+    public RPlayerPowerup playerPowerup;
+
+    private Player player;
+
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(playerNum);
+    }
+
+    private void OnEnable()
+    {
+        canShoot = true;
+        mineCount = 3;
+
+        foreach (var firepoint in firepoints)
+        {
+            GameObject mineGO = Instantiate(mine, firepoint.position, firepoint.rotation, transform);
+            mineGO.transform.localScale = firepoint.localScale;
+            mineGOs.Add(mineGO);
+        }
+    }
+
+    private void OnDisable()
+    {
+        canShoot = false;
+        mineGOs = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (!canShoot)
+        {
+            return;
+        }
+
+        if (mineCount < 1)
+        {
+            playerPowerup.DisablePowerup(powerupNum);
+            return;
+        }
+
+        if (player.GetButtonDown("Shoot"))
+        {
+            mineCount--;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject shotMine = mineGOs[mineCount - 1];
+        Rigidbody rb = shotMine.GetComponent<Rigidbody>();
+        shotMine.GetComponent<RMine>().isShot = true;
+        shotMine.transform.SetParent(null);
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        rb.AddForce(shotMine.transform.forward * initialForce, ForceMode.VelocityChange);
+    }
+}
