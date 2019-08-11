2af8d1388be744e20a75e09dfeddf0d2ba9cec0d
SamEmam <samhoffmann111@hotmail.com>
2019-08-12 00:06:41 +0200

    Update LoadingScreenCubeController.cs

--- Assets/Scripts/Game Tools/LoadingScreenCubeController.cs
+++ Assets/Scripts/Game Tools/LoadingScreenCubeController.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 public class LoadingScreenCubeController : MonoBehaviour
 {
     public List<Vector3> positions = new List<Vector3>();
-    public int index = 0;
     private Vector3 prevPos, curPos;
 
-    private void Start()
+    private void Update()
     {
         transform.position = Vector3.Lerp(prevPos, curPos, Time.deltaTime);
     }
 
-    void MoveToNextPosition()
+    public void MoveToNextPosition(int index)
     {
-        if (index == positions.Count - 1)
+        if (index == 0)
         {
-            index = 0;
             prevPos = positions[positions.Count - 1];
             curPos = positions[index];
         }
         else
         {
-            index++;
+            prevPos = positions[index - 1];
+            curPos = positions[index];
         }
     }
 }
