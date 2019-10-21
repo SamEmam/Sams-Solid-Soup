c706d861db659eea8217aebb0d7f9bca5b2f281d
SamEmam <samhoffmann111@hotmail.com>
2019-10-22 00:00:55 +0200

    Initialized Point To Point Space Race

---
+++ Assets/Scripts/Game Tools/Solid Soup/Race/SRaceMasterPositionAlt.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SRaceMasterPositionAlt : MonoBehaviour
+{
+    [SerializeField]
+    private RVehicleTypeSelector[] typeSelectors;
+    [SerializeField]
+    private SRacePlayerPosition[] players;
+    private List<SRacePlayerPosition> playerPositionList = new List<SRacePlayerPosition>();
+
+    private void Start()
+    {
+        for (int i = 0; i < typeSelectors.Length; i++)
+        {
+            if (typeSelectors[i].gameObject.activeInHierarchy)
+            {
+                players[i] = typeSelectors[i].GetVehicle().GetComponent<SRacePlayerPosition>();
+            }
+        }
+        StartCoroutine(DelayedStart());
+    }
+
+    IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (var player in players)
+        {
+            if (player.gameObject.activeInHierarchy)
+            {
+                playerPositionList.Add(player);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        BubbleSort();
+    }
+
+    void BubbleSort()
+    {
+        SRacePlayerPosition tempPlayer;
+        for (int i = 0; i < playerPositionList.Count; i++)
+        {
+            if (!playerPositionList[i])
+            {
+                continue;
+            }
+            playerPositionList[i].CalculatePoints();
+            for (int j = 0; j < playerPositionList.Count - 1; j++)
+            {
+                if (!playerPositionList[j])
+                {
+                    continue;
+                }
+                playerPositionList[j].CalculatePoints();
+
+                if (playerPositionList[j].points > playerPositionList[j + 1].points)
+                {
+
+                    tempPlayer = playerPositionList[j];
+                    playerPositionList[j] = playerPositionList[j + 1];
+                    playerPositionList[j + 1] = tempPlayer;
+
+                }
+            }
+        }
+
+        SetPosition();
+    }
+
+    void SetPosition()
+    {
+        for (int i = 0; i < playerPositionList.Count; i++)
+        {
+            playerPositionList[i].position = i + 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Game Tools/Solid Soup/Race/SRaceSpeedometerAlt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SRaceSpeedometerAlt : MonoBehaviour
+{
+    [SerializeField]
+    private SRaceFinishLine finishLine;
+    [SerializeField]
+    private float multiplier = 3;
+    [SerializeField]
+    private RVehicleTypeSelector typeSelector;
+    
+    private Rigidbody rb;
+    private SRacePlayerCheckpoint cp;
+    private SRacePlayerPosition pos;
+
+
+    private TextMeshProUGUI dial;
+    private float speed;
+
+    private void Start()
+    {
+        GameObject vehicle = typeSelector.GetVehicle();
+
+        rb = vehicle.GetComponent<Rigidbody>();
+        cp = vehicle.GetComponent<SRacePlayerCheckpoint>();
+        pos = vehicle.GetComponent<SRacePlayerPosition>();
+
+        dial = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        var velocity = rb.velocity.magnitude * multiplier;
+        speed = velocity;
+        dial.text = speed.ToString("F1") + " km/h \n";
+        if (finishLine.raceLaps > 1)
+        {
+            dial.text += (cp.laps + 1) + "/" + finishLine.raceLaps + " LAPS" + "\n";
+        }
+        dial.text += pos.positionString;
+    }
+}
